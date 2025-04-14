1cffc1202fca471f9bd7b17bc2e5f29711df401a
Cuautrix <hptablet098@gmail.com>
2025-04-13 22:53:42 -0600

    fix5

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
+# Etapa 1: Compilación
+FROM maven:3.9.4-eclipse-temurin-17 AS builder
+WORKDIR /app
+COPY . .
+RUN mvn clean package -DskipTests
+
+# Etapa 2: Imagen final liviana
 FROM amazoncorretto:17-alpine-jdk
-
-COPY target/system-0.0.1-SNAPSHOT.jar /api-v1.jar
-
-
-
-ENTRYPOINT ["java", "-jar", "/api-v1.jar"]
+WORKDIR /app
+COPY --from=builder /app/target/system-0.0.1-SNAPSHOT.jar app.jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/financial/system/core/models/dto/request/CorsConfig.java
+++ src/main/java/com/financial/system/core/models/dto/request/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4300", "https://prueba-front-sandy.vercel.app") // los que necesites
+                        .allowedOriginPatterns("*")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
